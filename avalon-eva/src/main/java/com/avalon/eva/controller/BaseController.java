a9cc48df1ed8fca3d20751e3c199d30dd35c55e9
EVANCHOW\Evan <evanzhou1992@163.com>
2019-07-03 18:59:39 +0800

    安全更新

--- avalon-eva/src/main/java/com/avalon/eva/controller/BaseController.java
+++ avalon-eva/src/main/java/com/avalon/eva/controller/BaseController.java
@@ -1,26 +1,25 @@
 package com.avalon.eva.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RefreshScope //开启更新功能
-@RequestMapping("api")
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class BaseController {
 
-    @Value("${from}")
-    private String fromValue;
-    /**
-     * 返回配置文件中的值
-     */
-    @GetMapping("/from")
-    @ResponseBody
-    public String returnFormValue(){
-        return fromValue;
+    @Value("${info.version}")
+    private String version;
+
+    @Value("${info.name}")
+    private String name;
+
+    @RequestMapping("/")
+    public String index(HttpServletRequest request){
+        request.setAttribute("name",version);
+        request.setAttribute("version",name);
+        return "index";
     }
+
 }
