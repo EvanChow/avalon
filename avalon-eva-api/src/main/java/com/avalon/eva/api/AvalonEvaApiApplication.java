1198f05f0972e5d3585bd95beeabefbb78ea2575
EvanChow <evanchow1993@gmail.com>
2019-07-18 01:16:28 +0800

    API

---
+++ avalon-eva-api/src/main/java/com/avalon/eva/api/AvalonEvaApiApplication.java
@@ -0,0 +1,13 @@
+package com.avalon.eva.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvalonEvaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvalonEvaApiApplication.class, args);
+    }
+
+}
